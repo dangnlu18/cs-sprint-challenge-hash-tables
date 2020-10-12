8106d7438b9c7014e0b0d353b112e3fd2200b570
dang lu <dangnlu18@gmail.com>
2020-10-11 17:58:23 -0700

    mvp met for ex1 ex3 ex4

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,18 @@
+cache = {}
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
+    for key, value in enumerate(weights):
+        cache[value] = key
 
+    for key, w in enumerate(weights):
+        if (limit- w) in cache:
+            if cache[limit - w] > key:
+                return(cache[limit -w ], key)
+            else:
+                return(key, cache[limit-w])
+ 
     return None
